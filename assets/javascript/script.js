bf1f13474df6522a6c516e19a628d480d9c07a96
ilqarsfv <ilqar.sofiyev.95@gmail.com>
2025-09-22 21:36:17 +0400

    v1.1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,39 @@
 $(document).ready(() => {
-  $("#steppedBtn").click(() => {
-    let valid = true;
+    $("#steppedBtn").click(() => {
+        let valid = true;
 
-    $("#formInputsOne input[required]").each(function () {
-      if ($(this).val().trim() === "") {
-        valid = false;
-        $(this).addClass("error");
-      } else {
-        $(this).removeClass("error");
-      }
+        if ($("#steppedBtn").text() === "Növbəti") {
+            $("#formInputsOne input[required]").each(function () {
+                if ($(this).val().trim() === "") {
+                    valid = false;
+                    $(this).addClass("error");
+                } else {
+                    $(this).removeClass("error");
+                }
+            });
+
+            if (valid) {
+                $("#steppedBtn").text("Göndər");
+                $("#formInputsOne").addClass("hidden");
+                $("#formInputsTwo").addClass("show");
+                $("#formLine").addClass("stepped");
+                $("#stepTwo").addClass("active");
+            }
+        } else if ($("#steppedBtn").text() === "Göndər") {
+            $("#formInputsTwo input[required]").each(function () {
+                if ($(this).val().trim() === "") {
+                    valid = false;
+                    $(this).addClass("error");
+                } else {
+                    $(this).removeClass("error");
+                }
+            });
+
+            if (valid) {
+                $("#formInputsTwo").removeClass("show").addClass("hidden");
+                $("#steppedBtn").hide();
+                $("#successForm").addClass("show");
+            }
+        }
     });
-
-    if (valid) {
-      $("#steppedBtn").text("Göndər");
-      $("#formInputsOne").addClass("hidden");
-      $("#formInputsTwo").addClass("show");
-      $("#formLine").addClass("stepped");
-      $("#stepTwo").addClass("active");
-    }
-  });
 });
